3672357cdd5621348c2e56d8427148eb3a9e79c9
iLynette <lynetteacholah@icloud.com>
2022-03-21 12:12:15 +0300

    Add an App.css file and style the input field

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -62,19 +62,21 @@ class TodoContainer extends React.Component {
         });
       };
       
-  render() {
-    return (
-      <div>
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodosList 
-          todos={this.state.todos} 
-          handleChangeProps={this.handleChange} 
-          deleteTodoProps={this.delTodo} 
-        />
-        
-     </div>
-    );
-  }
+      render() {
+        return (
+          <div className="container">
+            <div className="inner">
+              <Header />
+              <InputTodo addTodoProps={this.addTodoItem} />
+              <TodosList
+                todos={this.state.todos}
+                handleChangeProps={this.handleChange}
+                deleteTodoProps={this.delTodo}
+              />
+            </div>
+          </div>
+        );
+      }
+      
 }
 export default TodoContainer
