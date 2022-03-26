568fa43c77ec1fbf70d14666c94916863c3af7cb
iLynette <lynetteacholah@icloud.com>
2022-03-26 21:30:25 +0300

    Updates

---
+++ precache-manifest.2f3561c71f57a603ed60218af412234b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "729b028735977ef38138",
+    "url": "/todo_list_react/static/css/main.fee26076.chunk.css"
+  },
+  {
+    "revision": "729b028735977ef38138",
+    "url": "/todo_list_react/static/js/main.729b0287.chunk.js"
+  },
+  {
+    "revision": "16476deaf599d541754b",
+    "url": "/todo_list_react/static/js/1.16476dea.chunk.js"
+  },
+  {
+    "revision": "ad57f459cf1770509b43",
+    "url": "/todo_list_react/static/js/runtime~main.ad57f459.js"
+  },
+  {
+    "revision": "7b601447fb9a92f1e37fff0d98ffb883",
+    "url": "/todo_list_react/index.html"
+  }
+];
